093466ab9b9cf14ceacdef6ba48f2effa4f747ea
송영민 <jakgon@naver.com>
2023-10-25 19:43:08 +0900

    init: 프로젝트 생성

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "nagaza-server"
---
+++ src/test/kotlin/kr/nagaza/nagazaserver/NagazaServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.nagaza.nagazaserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NagazaServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
