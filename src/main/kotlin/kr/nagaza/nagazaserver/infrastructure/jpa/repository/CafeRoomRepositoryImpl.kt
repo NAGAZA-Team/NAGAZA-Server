e73ee9d3d6f74ed65e37ea4b5ab73e0ecfa91ec5
HyungJu <sungkisa@naver.com>
2024-02-25 14:31:10 +0900

    feat: 방 검색 querydsl로 만들자

--- src/main/kotlin/kr/nagaza/nagazaserver/infrastructure/jpa/repository/CafeRoomRepositoryImpl.kt
+++ src/main/kotlin/kr/nagaza/nagazaserver/infrastructure/jpa/repository/CafeRoomRepositoryImpl.kt
@@ -1,13 +1,20 @@
 package kr.nagaza.nagazaserver.infrastructure.jpa.repository
 
+import com.querydsl.jpa.impl.JPAQuery
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
 import kr.nagaza.nagazaserver.domain.model.CafeRoom
 import kr.nagaza.nagazaserver.domain.repository.CafeRoomRepository
+import kr.nagaza.nagazaserver.infrastructure.jpa.entity.QCafeEntity
+import kr.nagaza.nagazaserver.infrastructure.jpa.entity.QCafeRoomEntity
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
 class CafeRoomRepositoryImpl(
     private val jpaCafeRoomRepository: JpaCafeRoomRepository,
+    @PersistenceContext
+    private val entityManager: EntityManager,
 ) : CafeRoomRepository {
     override fun getAllRoomByCafeId(cafeId: String): List<CafeRoom> {
         return jpaCafeRoomRepository.findAllByCafeId(cafeId)
@@ -27,13 +34,36 @@ class CafeRoomRepositoryImpl(
         address2: String?,
         cafeId: String?,
     ): List<CafeRoom> {
-        return jpaCafeRoomRepository.search(
-            queryString = queryString,
-            genre = genre,
-            address1 = address1,
-            address2 = address2,
-            cafeId = cafeId,
-        ).map {
+        val cafeRoomEntity = QCafeRoomEntity.cafeRoomEntity
+        val cafeEntity = QCafeEntity.cafeEntity
+        val query =
+            JPAQuery<CafeRoom>(entityManager)
+                .select(cafeRoomEntity)
+                .from(cafeRoomEntity)
+                .leftJoin(cafeEntity).on(cafeRoomEntity.cafeId.eq(cafeEntity.cafeId))
+                .leftJoin(cafeRoomEntity.genre)
+
+        if (queryString != null) {
+            query.where(cafeRoomEntity.title.contains(queryString))
+        }
+
+        if (genre != null) {
+            query.where(cafeRoomEntity.genre.any().genreId.eq(genre))
+        }
+
+        if (address1 != null) {
+            query.where(cafeEntity.addressOne.eq(address1))
+        }
+
+        if (address2 != null) {
+            query.where(cafeEntity.addressTwo.eq(address2))
+        }
+
+        if (cafeId != null) {
+            query.where(cafeRoomEntity.cafeId.eq(cafeId))
+        }
+
+        return query.fetch().map {
             it.toModel()
         }
     }
